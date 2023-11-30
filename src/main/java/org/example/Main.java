3d4374e00f3323187cbe7ee26e00980b1134d742
maxnotmaximchik <maxsv_it@tuta.io>
2023-11-30 22:09:43 +0300

    project setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // lets the window properly close when the user
+        // clicks the close ("X" button)
+        window.setResizable(false); // cannot resize this window
+        window.setTitle("The philosopher's Journey"); // title of the game
+        window.setLocationRelativeTo(null); // will be displayed at the center of the screen
+        window.setVisible(true);
     }
 }
